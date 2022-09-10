a7699c2b1be7dc2130906b82c8a00c5458a3770b
Светлана Петровна Андрела <104157616+ASvitlana@users.noreply.github.com>
2022-09-10 20:09:02 +0300

    add Task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const imagesMarkup = createGalleryItemsEl(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', imagesMarkup);
+
+function createGalleryItemsEl(items) {
+    return galleryItems.map(({ preview,  original, description}) => {
+        return `
+            <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        `;
+    })
+    .join('');
+};
+
+let gallery = new SimpleLightbox(".gallery__item", {
+    captionsData: "alt",
+    captionDelay: 250,
+});
+
+
+

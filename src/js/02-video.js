95a322021fefe88de9d43eb973bd07b666be8cc9
Светлана Петровна Андрела <104157616+ASvitlana@users.noreply.github.com>
2022-09-15 22:09:11 +0300

    ДЗ-8, на перевірку

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,23 @@
 
 import Player from '@vimeo/player';
-import { throttle } from 'lodash';
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', throttle(onPlay, 1000));
-
-function onPlay({ seconds }) {
-    localStorage.setItem('videoplayer-current-time', seconds);
+function onPlay(time) {
+    localStorage.setItem(STORAGE_KEY, time.seconds);
 }
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+player.on('timeupdate', throttle(onPlay, 1000));
+
+if (localStorage.getItem(STORAGE_KEY)){
+    player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+}
+
+
+
+
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,38 +2,40 @@
 import throttle from 'lodash.throttle';
 
 
-const refs = {
-    form: document.querySelector('.feedback-form'),
-    inputedEmail: document.querySelector('input'),
-    inputedMessage: document.querySelector('textarea'),
-};
+const form = document.querySelector('.feedback-form');
 
 const LOCALSTORAGE_KEY = 'feedback-form-state';
 
 const formData = {};
 
-refs.form.addEventListener('submit', onFormSubmit);
-refs.form.addEventListener('input', throttle(onTextInput, 500));
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onTextInput, 500));
 
 populateInputedText();
 
 function onTextInput(event) {
-    // const formData = { email: refs.inputedEmail.value, message: refs.inputedMessage.value };
     formData[event.target.name] = event.target.value;
     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
 };
 
 function onFormSubmit(event) {
     event.preventDefault();
+    
     console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
     event.target.reset();
     localStorage.removeItem(LOCALSTORAGE_KEY);
 };
 
-function populateInputedText(event) {
-    const savedMessages = localStorage.getItem(LOCALSTORAGE_KEY);
+function populateInputedText() {
+    let savedMessages = localStorage.getItem(LOCALSTORAGE_KEY);
 
     if(savedMessages) {
-        formData[event.target.name] = event.target.value;
+        savedMessages = JSON.parse(savedMessages);
+        Object.entries(savedMessages).forEach(([name, value]) => {
+            form.elements[name].value = value;
+        })
     }
 }
+
+
+
